29fe6b5648f4c5c4954f057e7e6417a882f259f6
listenhayaku <94486789+listenhayaku@users.noreply.github.com>
2021-11-17 14:29:35 +0800

    Update and rename mycheck.py to check.py

--- check.py
+++ check.py
@@ -1,35 +1,36 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Nov 14 09:36:54 2021
-
 @author: user
 """
 import requests
 import time
 import sys
 
-def writelog(msg):
-    f = open("check.log",'a')
-    string = "["+time.ctime(time.time())+"]"+"  " + msg
+def writelog(url,msg):
+    f = open("/var/log/dirb_script/check.log",'a')
+    string = "["+time.ctime(time.time())+"] "+ url + msg
     f.write(string)
     
-def run(): 
-    url = "http://"+sys.argv[1]+":"+sys.argv[2]+"/"
+def run(url): 
+    
     try:
         r = requests.post(url)
     except:
-        writelog(url+" timeout\n")
+        writelog(url," timeout\n")
         return 0
     print("r.status_code: ",r.status_code)
+    
     if r.status_code >= 200 and r.status_code <= 399:
-        writelog(url+" is alive\n")
+        writelog(url," is alive\n")
         return 1
     else:
-        writelog(url+" code:"+str(r.status_code)+"\n")
+        writelog(url," code:"+str(r.status_code)+"\n")
         return 0
     
     
 if __name__ == "__main__":
-    ret = run()
-    print(ret)
-    exit(ret)
+	url = "http://"+sys.argv[1]+":"+sys.argv[2]+"/"
+	ret = run(url)
+	print(ret)
+	exit(ret)
